bb608bb9bb0ab603441774dcc0d5b1c169aa3134
dayas-kr-coder <dayaskr2005@gmail.com>
2024-06-23 00:05:56 +0530

    Create a MySQL Database + PDO First Steps

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
 
 require('functions.php');
-require('router.php');
+// require('router.php');
+
+$dsn = 'mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4';
+
+$pdo = new PDO($dsn, 'root', 'dayas1981');
+
+$statement = $pdo->prepare("select * from posts;");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
