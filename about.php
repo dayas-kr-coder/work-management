d670fdcf13899a7d6cbd0cda848bc90e80e257f2
dayas-kr-coder <dayaskr2005@gmail.com>
2024-06-22 23:13:52 +0530

    PHP Partials

--- about.php
+++ about.php
@@ -1,3 +1,5 @@
 <?php
 
-require('about.view.php');
+$heading = 'About Page';
+
+require('views/about.view.php');
--- index.php
+++ index.php
@@ -1,3 +1,5 @@
 <?php
 
-require('index.view.php');
+$heading = 'Home Page';
+
+require('views/index.view.php');
