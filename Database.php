da64c2bcb56462c89d875f392e76df0ea42597b4
dayas-kr-coder <dayaskr2005@gmail.com>
2024-06-23 00:39:10 +0530

    Environments and Configuration Flexibility

--- Database.php
+++ Database.php
@@ -4,11 +4,13 @@ class Database
 {
   public $connection;
 
-  public function __construct()
+  public function __construct($config, $username = 'root', $password = 'dayas1981')
   {
-    $dsn = 'mysql:host=localhost;port=3306;dbname=myapp;charset=utf8mb4';
+    $dsn = 'mysql:' . http_build_query($config, '', ';');
 
-    $this->connection = new PDO($dsn, 'root', 'dayas1981');
+    $this->connection = new PDO($dsn, $username, $password, [
+      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
   }
 
   public function query($query)
