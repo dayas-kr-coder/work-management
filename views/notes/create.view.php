2220504ee39d70a873683c0be90d8e79b801dae3
dayas-kr-coder <dayaskr2005@gmail.com>
2024-06-23 16:10:13 +0530

    Namespacing: What, Why, How?

--- views/notes/create.view.php
+++ views/notes/create.view.php
@@ -8,7 +8,7 @@
       <div class="w-full">
         <label for="body" class="block text-sm font-medium leading-6 text-gray-900">body</label>
         <div class="mt-2">
-          <textarea id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Here's is an idea for a note..."><?= $_POST['body'] ?? '' ?></textarea>
+          <textarea id="body" name="body" rows="3" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Here's is an idea for a note..."><?= $_POST['body'] ?? '' ?></textarea>
 
           <?php if (isset($errors['body'])) : ?>
             <p class="text-xs text-red-500 mt-2"><?= $errors['body'] ?></p>
