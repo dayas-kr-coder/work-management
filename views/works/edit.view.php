c9e87582d557691c40a04b08fec8cf586a08b0e9
dayas-kr-coder <dayaskr2005@gmail.com>
2024-07-05 16:30:31 +0530

    fix edit controller

--- views/works/edit.view.php
+++ views/works/edit.view.php
@@ -4,7 +4,7 @@
 
 <main>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    <form method="POST" action="/work">
+    <form method="POST" action="/work/update">
       <input type="hidden" name="_method" value="PATCH">
       <input type="hidden" name="id" value="<?= $work['id'] ?>">
 
